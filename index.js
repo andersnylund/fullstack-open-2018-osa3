931fb03d572273880261a07c2216a01107131c41
Anders Nylund <anders.nylund@aalto.fi>
2018-06-04 18:23:25 +0300

    Solve 3.4

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ app.get('/api/persons/:id', (req, res) => {
 	}
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(p => p.id === id)
+	if (person) {
+		persons = persons.filter(p => p.id !== id)
+		res.status(204).end()
+	} else {
+		res.status(404).end()
+	}
+})
+
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
